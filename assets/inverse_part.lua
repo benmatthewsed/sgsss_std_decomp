248a039b7ea92d01b351f9e69b32040f0710c830
Josiah King <josiahpjking@gmail.com>
2026-06-25 11:17:05 +0100

    crossclass lec

--- assets/inverse_part.lua
+++ assets/inverse_part.lua
@@ -1,7 +1,7 @@
 function Header(el)
     if el.level == 1 then
 --    table.insert(el.classes, "inverse")
-      el.attributes["data-background-color"] = '#25aeda'
+      el.attributes["data-background-color"] = '#006938'
       return el
     end
 end
